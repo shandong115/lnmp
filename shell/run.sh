0b63a751c7cd77af7d8af94e1c470e23065653ec
shandong115 <mr.danzhao@163.com>
2020-05-20 15:27:29 +0800

    modify run.sh

--- shell/run.sh
+++ shell/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "setup nginx php php-fpm php-mysql mariadb mariadb-server wordpress start ..."
-yum install -y nginx php php-fpm php-mysql mariadb mariadb-server wordpress
+yum install -y nginx 
+yum install -y php php-fpm php-mysql php-gd php-mbstring php-mcrypt
+yum install -y mariadb mariadb-server 
+yum install -y wordpress 
 echo "setup nginx php php-fpm php-mysql mariadb mariadb-server wordpress over"
 
 echo "create db user..."
@@ -17,6 +20,8 @@ echo "create db user over"
 echo "copy config file..."
 cp ~/wordpress/conf/wp-config.php /etc/wordpress/
 cp ~/wordpress/conf/nginx.conf /etc/nginx/
+chown -R nginx:nginx /usr/share/wordpress
+sed -i "s/apache/nginx/g" /etc/php-fpm.d/www.conf
 
 echo "start server..."
 systemctl start nginx
